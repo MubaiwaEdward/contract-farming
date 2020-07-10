2cb53cbe34348585628a6543b72d861e993c7e65
Linval <tlinvalchisoko@gmail.com>
2020-07-10 15:36:44 +0200

    created the contact cost class

---
+++ src/main/java/com/cb/contractfarming/contractcost/contractcost.java
@@ -0,0 +1,42 @@
+package com.cb.contractfarming.contractcost;
+import com.cb.contractfarming.common.BaseEntity;
+import com.cb.contractfarming.farmer.model.Farmer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contractcost", indexes = {@Index(name = "indx_contcts", columnList = "id", unique = true)})
+@Getter
+@Setter
+@ToString
+@Access(AccessType.FIELD)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class contractcost extends BaseEntity {
+
+    @Column(name = "contract_name", nullable = false, length = 45)
+    private String contractName;
+
+    @Column(name = "contarct_description",nullable = false,length = 225)
+    private String description;
+
+    @Column(name = "contract_type", nullable = false, length = 45)
+    private String type;
+    @Column(name = "price",nullable = false ,length = 10)
+    private  Double price;
+
+    @Column(name = "contract_start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "contract_end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "contractor_id", nullable = false, length = 45)
+    private String contractor;
+}
+
